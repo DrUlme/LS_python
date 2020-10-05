10691eb6e2300914a8feec87e071076a26890aef
Dr. Ulf Meerwald <ulf@meerwald.net>
2020-10-05 16:04:47 +0200

    LSglobal für globale Einstellungen an der Datenbank vorbei

---
+++ LSglobal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 14:57:53 2020
+
+@author: ulf
+"""
+
+DBName = "LS2020H.db"
+
+Jahr = 2020
+
+MeldeDir   = "Meldungen"
+StartXLS   = 'Startreihenfolge_H2020.xlsx'
+
+SQLiteFile = "LS2020H.db"
+
+TrzFiles = ["Start.trz", "3000.trz", "Ziel.trz"]
+Trz_dSec = [ 0, 0, 0]
+Trz_m  = [ 0, 3000, 6000]
+
